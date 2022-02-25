f5aa97a01102c00ae7b5d6bca526f9b0009863ab
Joaquin Vilchez <joaquinvilchez95@gmail.com>
2022-02-24 23:22:14 -0300

    UPDATE: Add new packages and fix problems

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,7 @@ export default {
   /*
   ** Nuxt.js dev-modules
   */
-  buildModules: [],
+  buildModules: ['@nuxt/typescript-build'],
   /*
   ** Nuxt.js modules
   */
--- pages/starter-page.vue
+++ pages/starter-page.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="row"><h3>Starter page</h3></div>
 </template>
-<script>
-  export default {
-    name: 'starter-page',
-    layout: 'starter',
-    data() {
-      return {};
-    }
-  };
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class extends Vue {
+  mounted() {
+    console.log('OK')
+  }
+}
 </script>
-<style></style>
